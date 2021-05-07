ea96a2c45a37ef4a0ca0839f22293f704bcd8be5
Laoa Raoand Jalal <laoa99@outlook.com>
2021-05-08 01:29:46 +0200

    graphs

--- script/Utils.py
+++ script/Utils.py
@@ -28,16 +28,21 @@ def read_yaml_file(path_to_yaml: str):
         print(e + ": Error reading the yaml file: " + path_to_yaml)
 
 
-def make_dataframe_row(metrics, setting: list, setting_id: str) -> dict:
+def make_dataframe_row(metrics, setting: list, feature_rep: str, setting_id: str) -> dict:
     report = metrics.get_classification_report()
     quantity_table = metrics.quantity_table()
     row = {
         "classifier": metrics.name,
         "setting_id": setting_id,
+        "Feature rep.": feature_rep,
         "settings": setting,
         "accuracy": report["accuracy"],
-        "macro_avg_precision": report["macro avg"]["precision"],
-        "macro_avg_recall": report["macro avg"]["recall"],
+        "macro_precision": report["macro avg"]["precision"],
+        "macro_recall": report["macro avg"]["recall"],
+        "weighted_precision": report["weighted avg"]["precision"],
+        "weighted_recall": report["weighted avg"]["recall"],
+        "macro_f1": report["macro avg"]["f1-score"],
+        "weighted_f1": report["weighted avg"]["f1-score"],
         "train_size": quantity_table["Train"].sum(),
         "test_size": quantity_table["Test"].sum(),
         "report_table": metrics.total_report_table(),
