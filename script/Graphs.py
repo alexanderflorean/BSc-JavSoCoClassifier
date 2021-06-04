0b7ccd0645dba904f2025b5be6333384e21be490
Alexander Florean <florean.alexander@gmail.com>
2021-06-04 11:03:25 +0200

    update

--- script/Graphs.py
+++ script/Graphs.py
@@ -55,7 +55,7 @@ def plot_line_graph(title: str, x_axis, y_axis: dict, x_axis_name, y_axis_name, 
         fig = plt.figure(figsize=(8, 8))
         ax1 = fig.add_subplot(111)
         plt.title(title, fontsize=20)
-        ax1.set_ylim(0, 1)
+        ax1.set_ylim(0.7, 1)
         plt.ylabel(y_axis_name, fontsize=15)
         plt.xlabel(x_axis_name, fontsize=15)
 
