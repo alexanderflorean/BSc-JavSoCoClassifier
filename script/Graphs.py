a6fe7a6fec06beca9f2940cf9c2cdd08bbdaab1a
Laoa Raoand Jalal <laoa99@outlook.com>
2021-06-08 15:04:33 +0200

    graphs fix

--- script/Graphs.py
+++ script/Graphs.py
@@ -54,59 +54,65 @@ def plot_line_graph(title: str, x_axis, y_axis: dict, x_axis_name, y_axis_name, 
     if type == "accuracy":
         fig = plt.figure(figsize=(8, 8))
         ax1 = fig.add_subplot(111)
-        plt.title(title, fontsize=20)
-        ax1.set_ylim(0.7, 1)
-        plt.ylabel(y_axis_name, fontsize=15)
-        plt.xlabel(x_axis_name, fontsize=15)
+        plt.title(title, fontsize=24)
+        ax1.set_ylim(0.6, 1)
+        plt.ylabel(y_axis_name, fontsize=20)
+        plt.xlabel(x_axis_name, fontsize=20)
 
-        ax1.scatter(x_axis, y_axis["naive"], s=50, c="b", marker="x", label="Naive-Bayes")
-        ax1.plot(x_axis, y_axis["naive"], c="b")
+        ax1.scatter(x_axis, y_axis["naive"], s=50, c="b", marker="s", label="Naive-Bayes")
+        ax1.plot(x_axis, y_axis["naive"], c="b", linewidth=2)
 
-        ax1.scatter(x_axis, y_axis["maxEnt"], s=50, c="r", marker="x", label="MaxEnt")
-        ax1.plot(x_axis, y_axis["maxEnt"], c="r")
+        ax1.scatter(x_axis, y_axis["maxEnt"], s=50, c="r", marker="o", label="MaxEnt")
+        ax1.plot(x_axis, y_axis["maxEnt"], c="r", linewidth=2)
 
-        ax1.scatter(x_axis, y_axis["svm"], s=50, c="g", marker="x", label="SVM")
-        ax1.plot(x_axis, y_axis["svm"], c="g")
+        ax1.scatter(x_axis, y_axis["svm"], s=50, c="grey", marker="x", label="SVM")
+        ax1.plot(x_axis, y_axis["svm"], c="grey", linewidth=2)
 
-        plt.legend(loc="lower right")
+        plt.legend(loc="lower right",
+                   prop={'size':15},
+                   labelspacing= 1.2)
         plt.show()
     elif type == "macro":
         fig = plt.figure(figsize=(8, 8))
         ax1 = fig.add_subplot(111)
-        plt.title(title, fontsize=20)
-        ax1.set_ylim(0, 1)
-        plt.ylabel(y_axis_name, fontsize=15)
-        plt.xlabel(x_axis_name, fontsize=15)
+        plt.title(title, fontsize=24)
+        ax1.set_ylim(0.6, 1)
+        plt.ylabel(y_axis_name, fontsize=20)
+        plt.xlabel(x_axis_name, fontsize=20)
 
-        ax1.scatter(x_axis, [x[0] for x in y_axis["naive"]], s=50, c="b", marker="x", label="Naive-Bayes")
-        ax1.plot(x_axis, [x[0] for x in y_axis["naive"]], c="b")
+        ax1.scatter(x_axis, [x[0] for x in y_axis["naive"]], s=50, c="b", marker="s", label="Naive-Bayes")
+        ax1.plot(x_axis, [x[0] for x in y_axis["naive"]], c="b", linewidth=2)
 
-        ax1.scatter(x_axis, [x[0] for x in y_axis["maxEnt"]], s=50, c="r", marker="x", label="MaxEnt")
-        ax1.plot(x_axis, [x[0] for x in y_axis["maxEnt"]], c="r")
+        ax1.scatter(x_axis, [x[0] for x in y_axis["maxEnt"]], s=50, c="r", marker="o", label="MaxEnt")
+        ax1.plot(x_axis, [x[0] for x in y_axis["maxEnt"]], c="r", linewidth=2)
 
-        ax1.scatter(x_axis, [x[0] for x in y_axis["svm"]], s=50, c="g", marker="x", label="SVM")
-        ax1.plot(x_axis, [x[0] for x in y_axis["svm"]], c="g")
+        ax1.scatter(x_axis, [x[0] for x in y_axis["svm"]], s=50, c="grey", marker="x", label="SVM")
+        ax1.plot(x_axis, [x[0] for x in y_axis["svm"]], c="grey", linewidth=2)
 
-        plt.legend(loc="lower right")
+        plt.legend(loc="lower right",
+                   prop={'size': 15},
+                   labelspacing=1.2)
         plt.show()
     elif type == "weighted":
         fig = plt.figure(figsize=(8, 8))
         ax1 = fig.add_subplot(111)
-        plt.title(title, fontsize=20)
-        ax1.set_ylim(0, 1)
-        plt.ylabel(y_axis_name, fontsize=15)
-        plt.xlabel(x_axis_name, fontsize=15)
+        plt.title(title, fontsize=24)
+        ax1.set_ylim(0.6, 1)
+        plt.ylabel(y_axis_name, fontsize=20)
+        plt.xlabel(x_axis_name, fontsize=20)
 
-        ax1.scatter(x_axis, [x[1] for x in y_axis["naive"]], s=50, c="b", marker="x", label="Naive-Bayes")
-        ax1.plot(x_axis, [x[1] for x in y_axis["naive"]], c="b")
+        ax1.scatter(x_axis, [x[1] for x in y_axis["naive"]], s=50, c="b", marker="s", label="Naive-Bayes")
+        ax1.plot(x_axis, [x[1] for x in y_axis["naive"]], c="b", linewidth=2)
 
-        ax1.scatter(x_axis, [x[1] for x in y_axis["maxEnt"]], s=50, c="r", marker="x", label="MaxEnt")
-        ax1.plot(x_axis, [x[1] for x in y_axis["maxEnt"]], c="r")
+        ax1.scatter(x_axis, [x[1] for x in y_axis["maxEnt"]], s=50, c="r", marker="o", label="MaxEnt")
+        ax1.plot(x_axis, [x[1] for x in y_axis["maxEnt"]], c="r" , linewidth=2)
 
-        ax1.scatter(x_axis, [x[1] for x in y_axis["svm"]], s=50, c="g", marker="x", label="SVM")
-        ax1.plot(x_axis, [x[1] for x in y_axis["svm"]], c="g")
+        ax1.scatter(x_axis, [x[1] for x in y_axis["svm"]], s=50, c="grey", marker="x", label="SVM")
+        ax1.plot(x_axis, [x[1] for x in y_axis["svm"]], c="grey", linewidth=2)
 
-        plt.legend(loc="lower right")
+        plt.legend(loc="lower right",
+                   prop={'size': 15},
+                   labelspacing=1.2)
         plt.show()
 
 # plots the classification report table
